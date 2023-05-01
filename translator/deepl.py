f626b3bbc00d4fa9a0ed655b64abfdfe5ed8b16c
tsc-diy <tsctan@foxmail.com>
2023-05-01 14:58:26 +0800

    add qps for DeepL

--- translator/deepl.py
+++ translator/deepl.py
@@ -1,13 +1,14 @@
 import aiohttp
 import asyncio
+from aiolimiter import AsyncLimiter
 
 
 class Translator:
     def __init__(self, conf):
         self.conf = conf
+        self.limiter = AsyncLimiter(conf['qps'], 1)
 
-    @staticmethod
-    async def translate_text(text, target_language, api_key):
+    async def translate_text(self, text, target_language, api_key):
         url = "https://api-free.deepl.com/v2/translate"
         headers = {"Authorization": f"DeepL-Auth-Key {api_key}"}
         data = {
@@ -16,13 +17,14 @@ class Translator:
         }
 
         async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=headers, data=data) as response:
-                if response.status != 200:
-                    raise Exception(f"DeepL API request failed with status code {response.status}: {await response.text()}")
+            async with self.limiter:
+                async with session.post(url, headers=headers, data=data) as response:
+                    if response.status != 200:
+                        raise Exception(f"DeepL API request failed with status code {response.status}: {await response.text()}")
 
-                response_json = await response.json()
-                translated_text = response_json["translations"][0]["text"]
-                return translated_text
+                    response_json = await response.json()
+                    translated_text = response_json["translations"][0]["text"]
+                    return translated_text
 
     async def translate(self, text, to_english=True):
         if to_english:
@@ -33,8 +35,11 @@ class Translator:
 
 # Example usage
 async def main():
-    text = "你好"
-    conf = {"api_key": "your_api_key"}
+    text = """测试:
+def abc():
+    treturn '你好'
+结束"""
+    conf = {"api_key": "your_api_key", "qps": 1.}
     translator = Translator(conf)
 
     translated_text = await translator.translate(text)
